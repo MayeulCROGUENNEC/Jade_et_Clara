8b6e1f8353490d751760b5bd18e8b1973b19bf2b
Mayeul CROGUENNEC <ma.croguennec@orange.fr>
2022-08-21 17:25:43 +0200

    modif menus

---
+++ carousel/carousel.js
@@ -0,0 +1,55 @@
+
+//change prev icon on hover
+function prehover() {
+    const prev = document.getElementById('prev');
+    prev.setAttribute('src', 'https://img.icons8.com/dotty/80/000000/circled-chevron-left.png');
+  }
+  
+  function prenothover() {
+    const prev = document.getElementById('prev');
+    prev.setAttribute('src', 'https://img.icons8.com/ios-glyphs/30/000000/circled-chevron-left.png');
+  }
+  
+  //change next icon on hover
+  function nexthover() {
+    const next = document.getElementById('next');
+    next.setAttribute('src', 'https://img.icons8.com/dotty/80/000000/circled-chevron-right.png');
+  }
+  
+  function nextnothover() {
+    const next = document.getElementById('next');
+    next.setAttribute('src', 'https://img.icons8.com/ios-glyphs/30/000000/circled-chevron-right.png');
+  }
+  
+  //Frames
+  
+  /* set initial index to 0 */
+  let startFrame = 0;
+  showFrames(startFrame);
+  
+  /*move to next frame by increasing the index */
+  function nextFrame() {
+      showFrames(startFrame += 1);
+  }
+  
+  /* move to previous frame by decreasing the index */
+  function prevFrame() {
+      showFrames(startFrame -= 1);  
+  }
+  
+  
+  function showFrames(changeTo) {
+      let slides = document.getElementsByClassName("slide");
+      
+      if (changeTo > slides.length) {
+        startFrame = 1
+      }
+      if (changeTo < 1) {
+          startFrame = slides.length
+      }
+    
+      for (let slide of slides) {
+          slide.style.display = "none";
+      }   
+      slides[startFrame - 1].style.display = "block"; 
+  }
